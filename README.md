ce21e28d81c7b6ec273ad368f6416b06265dd8dd
Taha Ansari <taha.m.ansari@gmail.com>
2024-02-26 22:53:37 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# unsplash-nextjs
+
+:camera: unsplash search using next.js
+
+![](nextjs-logo.svg)
+
+- [x] Stateful Components
+- [x] Loading Feedback
+- [x] Async API Requests
+- [x] API Key Seperated Using `.env.local`
+- [x] Conditional Rendering
+- [x] Virtual DOM
+- [x] Cascading Grid
+
+requirements: [unsplash developer api key](https://unsplash.com/developers)
+
+add unsplash developer key to `.env.local` file in base of project
+
+    NEXT_PUBLIC_API_CLIENTID="insert-api-key-here"
+
+created as a working example of next.js & is based on [unsplash-js-search
+](https://github.com/equk/unsplash-js-search) (unsplash search using vanillajs)
+
+## unsplash references
+
+- https://unsplash.com/documentation
+- https://unsplash.com/api-terms
+
+## screenshot
+
+![](screenshot.jpg)
+
+# Contact
+
+Website: https://equk.co.uk
+
+Twitter: [@equilibriumuk](https://twitter.com/equilibriumuk)
+
+### notes
+
+> Unsplash API Terms (Attribution)
+>
+> Each time you or your Developer App displays a Photo, your Developer App must attribute Unsplash, the Unsplash photographer, and contain a link back to the photographer’s Unsplash profile.
---
+++ pages/index.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+
+import Masonry from 'react-masonry-css';
+import api from './api/unsplash';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Unsplash Search',
+      imageSearch: '',
+      loading: false,
+      images: [],
+    };
+  }
+
+  imageSearchChanged(event) {
+    this.setState({
+      imageSearch: event.target.value,
+    });
+  }
+
+  formSubmitted(event) {
+    event.preventDefault();
+
+    this.setState({
+      loading: true,
+      images: [],
+    });
+
+    api.search(this.state.imageSearch).then((images) => {
+      this.setState({
+        loading: false,
+        images,
+      });
+    });
+  }
+
+  render() {
+    const { title, imageSearch, loading, images } = this.state;
+    return (
+      <div className="App">
+        <h1>{title}</h1>
+        <form onSubmit={(event) => this.formSubmitted(event)}>
+          <label htmlFor="imageSearch">Image Search</label>
+          <input
+            className="searchBox"
+            type="text"
+            id="imageSearch"
+            name="imageSearch"
+            value={imageSearch}
+            onChange={(event) => this.imageSearchChanged(event)}
+          />
+          <button type="submit">Search</button>
+        </form>
+        {loading ? (
+          <img
+            src="img/loading.svg"
+            alt="api_loading"
+            className="api_loading"
+          />
+        ) : (
+          ''
+        )}
+        <section className="images">
+          <Masonry
+            breakpointCols="4"
+            className="gallery-grid"
+            columnClassName="gallery-grid_column"
+          >
+            {images.map((image) => (
+              <div className="ImageResult">
+                <img key={image.id} src={image.urls.regular} alt="" />
+                <a
+                  href={image.links.html}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="view_link"
+                >
+                  View on Unsplash
+                </a>
+                <a
+                  href={image.user.links.html}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="user_link"
+                >
+                  Photo by: {image.user.name}
+                </a>
+              </div>
+            ))}
+          </Masonry>
+        </section>
+        <div className="createdby">
+          <p>
+            created by <a href="https://github.com/equk">equk</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
